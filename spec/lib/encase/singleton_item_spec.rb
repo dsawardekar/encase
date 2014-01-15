b5b1be05779f5e2acbf08eab2c14184d71961560
Darshan Sawardekar <darshan@sawardekar.org>
2014-01-15 16:46:46 +0530

    Adds SingletonItem and tests.

---
+++ spec/lib/encase/singleton_item_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require 'encase/singleton_item'
+
+module Encase
+  class MySingletonClass
+  end
+
+  describe SingletonItem do
+    let(:container) {
+      c = double()
+      c.stub(:inject).with(anything()) { true }
+      c
+    }
+
+    let(:singleton_item) {
+      SingletonItem.new(container, :lorem, MySingletonClass)
+    }
+
+    it 'returns the same object on every fetch' do
+      singleton_item.reify()
+
+      instance1 = singleton_item.fetch()
+      instance2 = singleton_item.fetch()
+
+      expect(instance1).to eq(instance2)
+    end
+  end
+end
